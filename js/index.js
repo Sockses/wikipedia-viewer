a0b2849532d61e11ef3e63ad64f9839d32e8de91
Aaron Porter <aaron.w.porter@gmail.com>
2016-06-20 16:04:00 -0700

    update script to grab info from mediawiki api; add comments

--- js/index.js
+++ js/index.js
@@ -5,21 +5,30 @@ $(document).ready(function() {
     $("#resultsList").html("");
 
     $.ajax({
-            type: "GET",
-            url: "https:en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&prop=links&srsearch=" + encodeURIComponent($("#wikiQuery").val()) + "&callback=?",
-            contentType: "application/json; charset=utf-8",
-            async: false,
-            dataType: "json",
-            success: function (data, textStatus, jqXHR) {
-              var results = [];
-              data.query.search.forEach(function(result) {
-                results.push('<a href="' + "#" + '" class="list-group-item active">' + '<h4 class="list-group-item-heading">' + result.title + '</h4>' + '<p class="list-group-item-text">' + result.snippet + '</p>' + '</a>');
-              });
-              $("#resultsList").append(results.join(""));
-            },
-            error: function (errorMessage) {
-            }
-          });
-          $("#resultsList").show();
+      type: "GET",
+      url: "https:en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&generator=search&exsentences=2&exlimit=10&exintro=1&gsrsearch=" + encodeURIComponent($("#wikiQuery").val()) + "&callback=?",
+      dataType: "json",
+      success: function (data) {
+
+        var results = [];
+
+        // loop through each object
+        for (var key in data.query.pages) {
+          // skip loop if the property is from prototype
+          if (!data.query.pages.hasOwnProperty(key)) continue;
+
+          // push the data to the results array
+          results.push('<a href="https:en.wikipedia.org/?curid=' + data.query.pages[key].pageid + '" class="list-group-item active">' + '<h4 class="list-group-item-heading">' + data.query.pages[key].title + '</h4>' + '<p class="list-group-item-text">' + data.query.pages[key].extract + '</p>' + '</a>');
+        }
+
+        // give the results array to the list group
+        $("#resultsList").html(results.join(""));
+      },
+      error: function (data) {
+        console.log(data.statusText);
+      }
+    });
+
+    $("#resultsList").show();
   });
 });
